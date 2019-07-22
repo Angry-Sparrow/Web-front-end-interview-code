d848d3b27b25bdf77796a3a6ec610ce6bd529f6b
lee <905160228@qq.com>
2019-07-22 15:31:21 +0800

    实现柯里化，第一个写法有问题，待修改

--- 实现柯里化.js
+++ 实现柯里化.js
@@ -1,17 +1,60 @@
-function Animal(){}
-Animal.prototype.species = "动物";
-function Cat(name,color){
-    this.name  = name;
-    this.color = color;
+// 什么是柯里化？
+// 柯里化是把一个多参数函数转换为一个嵌套的一元函数的过程
+
+// =============普通写法未调通版o(╥﹏╥)o请看下面几个版本===============================
+function curry(){
+    var args = Array.prototype.slice.call(arguments);
+    var fn = function(){
+        // 这里形成了死循环
+        var newArgs = args.concat(Array.prototype.slice.call(arguments));
+        return multi.apply(this,newArgs);
+    };
+    fn.toString = function(){
+        return args.reduce(function(a,b){
+            return a * b;
+        })
+    };
+    return fn;
 }
-function extend2(Child,Parent){
-    var p = Parent.prototype;
-    var c = Child.prototype;
-    for(var i in p){
-        c[i] = p[i];
-    }
-    c.uber = p;
+function multiFn(a,b,c){
+    return a * b * c;
 }
-extend2(Cat,Animal);
-var cat1 = new Cat("毛毛","黑色");
-console.log("cat1当前种类：" + cat1.species);
+var multi = curry(multiFn);
+multi(2)(3)(4);
+multi(2,3,4);
+multi(2)(3,4);
+multi(2,3)(4);
+
+// =============Es6写法大佬版===============================
+// const curry = (fn, arr = []) => (...args) => (
+//     arg => arg.length === fn.length
+//         ? fn(...arg)
+//         : curry(fn,arg)
+// ) ([...arr, ...args]);
+// function fn(a,b,c){
+//     return a * b * c;
+// }
+// console.log(curry(fn)(1)(2)(3));
+// console.log(curry(fn)(2,2,3));
+
+// =============Es6写法逻辑清晰版===============================
+// let curry = (fn) => {
+//     if(typeof fn !== 'function'){
+//         console.log('No function provided');
+//     }
+//     return function curriedFn(...args){
+//         if(args.length < fn.length){
+//             return function(){
+//                 let arr = [].slice.call(arguments);
+//                 // console.log(arr); // 用来测试
+//                 return curriedFn.apply(null,args.concat(arr));
+//             }
+//         }
+//         return fn.apply(null,args);
+//     }
+// };
+// function multiFn(a,b,c){
+//     return a * b * c;
+// }
+// console.log(curry(multiFn)(1)(2)(3));
+// console.log(curry(multiFn)(2,2,3));
