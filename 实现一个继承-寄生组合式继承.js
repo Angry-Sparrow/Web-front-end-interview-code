1837a0d66dd24810d92a5bcde6adb5ad3118c3c6
lee <905160228@qq.com>
2019-07-22 14:09:52 +0800

    添加寄生组合式继承-实现继承里也包含了此方法，这里重写

--- 实现一个继承-寄生组合式继承.js
+++ 实现一个继承-寄生组合式继承.js
@@ -10,4 +10,26 @@ function Parent(name){
 }
 Parent.prototype.sayName  = function(){
     console.log('parent name : ' ,this.name);
+};
+function Child(name,parentName){
+    Parent.call(this.parentName);
+    this.name = name;
 }
+// 空的中间函数
+function create(proto){
+    function F(){}
+    F.prototype = proto;
+    return new F();
+}
+//将Child的prototype指向Parent的prototype
+Child.prototype = create(Parent.prototype);
+// Child.prototype.sayName = function(){
+//     console.log('child name:' ,this.name);
+// };
+Child.prototype.constructor = Child;
+//创建Parent实例
+var parent = new Parent('father');
+//给parent添加sayName原型属性
+parent.sayName();  // parent name :  father
+var child = new Child('son','father');
+child.sayName();   // parent name :  son
