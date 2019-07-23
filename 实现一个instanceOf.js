d9055f692623d6eaf27413197fb3c59d6d56f949
lee <905160228@qq.com>
2019-07-23 09:25:51 +0800

    实现instanceOF

---
+++ 实现一个instanceOf.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: lee
+ * @Date:2019/7/22 22:03
+ * @Last Modified by:lee
+ * @Last Modified time:2019/7/22 22:03
+ */
+
+// instanceof运算符用于测试构造函数的prototype属性是否出现在对象的原型链中的任何位置
+
+function instanceOf(left,right){
+    let proto  = left.__proto__;
+    let prototype = right.prototype;
+    while(true){
+        if(proto == null) return false;
+        if(proto == prototype) return true;
+        proto = proto.__proto__;
+    }
+}
